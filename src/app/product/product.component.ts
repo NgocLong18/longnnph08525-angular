7b86f9a6b5947c8da57fbfbfbd76a770beb496cb
NgocLong18 <akaishuu1357@gmail.com>
2020-03-17 13:58:02 +0700

    4 files modified, 1 added

--- src/app/product/product.component.ts
+++ src/app/product/product.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-
+import {Product} from '../Product';
 @Component({
   selector: 'app-product',
   templateUrl: './product.component.html',
@@ -10,6 +10,20 @@ export class ProductComponent implements OnInit {
   constructor() { }
 
   ngOnInit() {
+  }
+  product: Product = {
+    id: 1,
+    name: "Item 1",
+    price: 24.99 ,
+    desc: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Amet numquam aspernatur!",
+    status: false,
+    img: 'https://dummyimage.com/700x400/000/fff'
+  }
+  changeStatus(){
+    this.product.status = !this.product.status;
+  }
+  changeName(e){
+    this.product.name = e.target.value;
   }
 
 }
